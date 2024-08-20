dc6bfba069698a071b484947eb95cba53c5998d6
Hugogb12 <22302824@aluno.cotemig.com.br>
2024-08-20 18:39:51 -0300

    Add files via upload

---
+++ JogosAPI-main/src/pages/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <>
+        <footer className={styles.footer}>
+            FEITO P0R IAN LUCCA E HUGO LUIZ
+        </footer>
+        </>
+    )
+}
+
+export default Footer
